3a74a97424ca0881fdaf70ce0be88c1d91b42d36
Tahneet Kanwal <99545467+tahneetK13@users.noreply.github.com>
2023-07-05 00:21:35 +0530

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -31,7 +31,7 @@ const SocialLinks = () => {
               Mail <HiOutlineMail size={30} />
             </>
           ),
-          href: "mailto:tahneetkanwal01@gmail.com",
+          href: "mailto: tahneetkanwal01@gmail.com",
         },
         {
           id: 4,
