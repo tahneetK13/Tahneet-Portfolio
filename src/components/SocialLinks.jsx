4e1797813e7e9a6ef6e06a3d7b47e67520a27fe7
Tahneet Kanwal <99545467+tahneetK13@users.noreply.github.com>
2023-07-10 17:28:14 +0530

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -47,7 +47,7 @@ const SocialLinks = () => {
       ];
     
       return (
-        <div className=" lg:flex flex-col top-[35%] left-0 fixed ">
+        <div className=" lg:flex flex-col top-[60%] left-0 fixed ">
           <ul>
             {links.map(({ id, child, href, style, download }) => (
               <li
